7329f399cd39c3c44c1e69354e9d6e5aabc9927d
馒头饭 <mhjlw@126.com>
2023-05-02 17:18:17 +0800

    :art:Replace double quotes with single quotes

--- src/Auth/BaiduCloudAuth.php
+++ src/Auth/BaiduCloudAuth.php
@@ -4,7 +4,7 @@ use YZhanGateway\Exception\AuthException;
 class BaiduCloudAuth {
   private $ak;
   private $sk;
-  private $version = "1";
+  private $version = '1';
   private $timestamp;
   private $expiration = 1800;
   private $method;
@@ -67,75 +67,75 @@ class BaiduCloudAuth {
   public function genAuthorization()
   {
       $signature = $this->genSignature();
-      $authStr = "bce-auth-v" . $this->version . "/" .
-          $this->ak . "/" . $this->timestamp . "/" .
-          $this->expiration . "/" . $this->getSignedHeaderNames() . "/" . $signature;
+      $authStr = 'bce-auth-v' . $this->version . '/' .
+          $this->ak . '/' . $this->timestamp . '/' .
+          $this->expiration . '/' . $this->getSignedHeaderNames() . '/' . $signature;
       return $authStr;
   }
 
   public function genSignature()
   {
       if (empty($this->method)) {
-          throw new AuthException("method is null or empty");
+          throw new AuthException('method is null or empty');
       }
       $signingKey = $this->genSigningKey();
-      $this->signerLog("signingKey:" . $signingKey, __LINE__, __FILE__);
+      $this->signerLog('signingKey:' . $signingKey, __LINE__, __FILE__);
       $authStr = $this->method . "\n" .
           $this->getCanonicalURI() . "\n" .
           $this->getCanonicalParam() . "\n" .
           $this->getCanonicalHeaders();
-      $this->signerLog("auth str:" . $authStr, __LINE__, __FILE__);
+      $this->signerLog('auth str:' . $authStr, __LINE__, __FILE__);
       return $this->sha256($signingKey, $authStr);
   }
 
   public function genSigningKey()
   {
       if (empty($this->ak)) {
-          throw new AuthException("Access key is null or empty");
+          throw new AuthException('Access key is null or empty');
       }
       if (empty($this->sk)) {
-          throw new AuthException("Secret key is null or empty");
+          throw new AuthException('Secret key is null or empty');
       }
       if (empty($this->version)) {
-          throw new AuthException("Version is null or empty");
+          throw new AuthException('Version is null or empty');
       }
       if (empty($this->timestamp)) {
-          throw new AuthException("Timestamp is null or empty");
+          throw new AuthException('Timestamp is null or empty');
       }
       if (empty($this->expiration)) {
-          throw new AuthException("Expiration is null or empty");
+          throw new AuthException('Expiration is null or empty');
       }
-      $authStr = "bce-auth-v" . $this->version . "/" . $this->ak . "/" .
-          $this->timestamp . "/" . $this->expiration;
+      $authStr = 'bce-auth-v' . $this->version . '/' . $this->ak . '/' .
+          $this->timestamp . '/' . $this->expiration;
       return $this->sha256($this->sk, $authStr);
   }
 
   public function getCanonicalParam()
   {
     if (empty($this->params)) {
-        return "";
+        return '';
     }
     $arryLen = count($this->params);
-    $canonicalParams = "";
+    $canonicalParams = '';
     foreach ($this->params as $key => $value) {
         if (is_array($value)) {
             $num = count($value);
             if (count($value) == 0) {
-                $canonicalParams = $canonicalParams . $key . "=";
+                $canonicalParams = $canonicalParams . $key . '=';
             } else {
                 foreach ($value as $item) {
-                    $canonicalParams = $canonicalParams . $key . "=" . $item;
+                    $canonicalParams = $canonicalParams . $key . '=' . $item;
                     if ($num > 1) {
-                        $canonicalParams = $canonicalParams . "&";
+                        $canonicalParams = $canonicalParams . '&';
                         $num--;
                     }
                 }
             }
         } else {
-            $canonicalParams = $canonicalParams . $key . "=" . $value;
+            $canonicalParams = $canonicalParams . $key . '=' . $value;
         }
         if ($arryLen > 1) {
-            $canonicalParams = $canonicalParams . "&";
+            $canonicalParams = $canonicalParams . '&';
             $arryLen--;
         }
     }
@@ -145,27 +145,27 @@ class BaiduCloudAuth {
   public function getCanonicalURI()
   {
     if (empty($this->uri)) {
-        throw new AuthException("Uri is null or empty");
+        throw new AuthException('Uri is null or empty');
     }
     $newUri = $this->dataEncode($this->uri, true);
-    if (strpos($newUri, "/") === 0) {
+    if (strpos($newUri, '/') === 0) {
         return $newUri;
     }
-    return "/" . $newUri;
+    return '/' . $newUri;
   }
 
   public function getCanonicalHeaders()
   {
-    if (empty($this->headers) || !array_key_exists("host", $this->headers)) {
-        throw new AuthException("host not in headers");
+    if (empty($this->headers) || !array_key_exists('host', $this->headers)) {
+        throw new AuthException('host not in headers');
     }
-    $canonicalHeaders = "";
+    $canonicalHeaders = '';
     $strArry = array();
     foreach ($this->headers as $key => $value) {
         if (empty($value)) {
             continue;
         }
-        $strArry[] = $this->dataEncode($key, false) . ":" . $value;
+        $strArry[] = $this->dataEncode($key, false) . ':' . $value;
     }
     $arryLen = count($strArry);
     for ($i = 0; $i < $arryLen; $i++) {
@@ -186,15 +186,15 @@ class BaiduCloudAuth {
   private function dataEncode($data, $isPath)
   {
     if (empty($data)) {
-        return "";
+        return '';
     }
-    $encode = mb_detect_encoding($data, array("ASCII", "UTF-8", "GB2312", "GBK", "BIG5"));
-    if ($encode != "UTF-8") {
+    $encode = mb_detect_encoding($data, array('ASCII', 'UTF-8', 'GB2312', 'GBK', 'BIG5'));
+    if ($encode != 'UTF-8') {
         $data = mb_convert_encoding($data, 'utf-8', $encode);
     }
     $encodeStr = rawurlencode($data);
     if ($isPath) {
-        $encodeStr = str_replace("%2F", "/", $encodeStr);
+        $encodeStr = str_replace('%2F', '/', $encodeStr);
     }
     return $encodeStr;
   }
@@ -223,7 +223,7 @@ class BaiduCloudAuth {
         return $newArray;
     }
     foreach ($params as $key => $value) {
-        if (empty($key) || strtolower($key) == "authorization") {
+        if (empty($key) || strtolower($key) == 'authorization') {
             continue;
         }
         if (is_array($value)) {
@@ -244,11 +244,11 @@ class BaiduCloudAuth {
   private function getSignedHeaderNames()
   {
     $arryLen = count($this->headers);
-    $headerNames = "";
+    $headerNames = '';
     foreach ($this->headers as $key => $value) {
         $headerNames = $headerNames . $key;
         if ($arryLen > 1) {
-            $headerNames = $headerNames . ";";
+            $headerNames = $headerNames . ';';
             $arryLen--;
         }
     }
@@ -258,7 +258,7 @@ class BaiduCloudAuth {
   private function signerLog($content, $line, $file)
   {
     if ($this->needLog) {
-        error_log($file . ":" . $line . ":[" . $content . "]\n", 3, "./signer_log");
+        error_log($file . ':' . $line . ':[' . $content . ']\n', 3, './signer_log');
     }
   }
 }
