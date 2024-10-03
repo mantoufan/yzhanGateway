fc1167e1bf342112aaac65c002b6bd1ff20be291
mantoufan <mhjlw@126.com>
2024-10-04 03:29:49 +0800

    :new: add timeout

--- src/Tool/ClientTool.php
+++ src/Tool/ClientTool.php
@@ -6,13 +6,13 @@ class ClientTool {
     curl_setopt_array($ch, array(
       CURLOPT_URL => $params['url'],
       CURLOPT_CUSTOMREQUEST => $params['method'],
-      CURLOPT_HTTPHEADER => empty($params['httpHeaders']) ? array() : array_map(function($v, $k) {
+      CURLOPT_HTTPHEADER => empty($params['httpHeaders']) ? array() : array_map(function ($v, $k) {
         return $k . ':' . $v;
-      }, array_values((array)$params['httpHeaders']), array_keys((array)$params['httpHeaders'])),
+      }, array_values((array) $params['httpHeaders']), array_keys((array) $params['httpHeaders'])),
       CURLOPT_POST => $params['method'] === 'POST',
       CURLOPT_POSTFIELDS => empty($params['postFields']) ? null : $params['postFields'],
       CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_TIMEOUT => 6,
+      CURLOPT_TIMEOUT => $params['timeout'] ?? 6,
       CURLOPT_SSL_VERIFYHOST => false,
       CURLOPT_SSL_VERIFYPEER => false,
       CURLOPT_HEADER => true,
@@ -22,7 +22,7 @@ class ClientTool {
     return array(
       'code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
       'header' => substr($response, 0, $header_size),
-      'body' => substr($response, $header_size)
+      'body' => substr($response, $header_size),
     );
   }
 }
